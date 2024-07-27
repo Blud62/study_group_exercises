5aafcb09bced98d4f8273d9a18177f9a6299db80
Blud62 <bludmedia@sbcglobal.net>
2024-07-26 18:42:33 -0700

    second commit

--- conditional_weather.py
+++ conditional_weather.py
@@ -15,7 +15,7 @@ weather = 'snowy'
 #     print("Let's stay inside. if weather's value is anything else")
 
 match weather:
-    case 'rainy':
+    case 'stormy':
         print("Grab your umbrella. if weather's value is 'rainy'")
     case 'sunny':
         print("It's a beautiful day! if weather's value is 'sunny")
